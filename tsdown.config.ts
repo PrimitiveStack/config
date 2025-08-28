2b2cafb9b278f8c7e34f264ce12a8c91c21a74d7
Tiago Jacinto <tiagojacintodev@gmail.com>
2025-08-28 14:14:41 +0100

    feat: define configs

---
+++ tsdown.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: ['./src/index.ts'],
+  platform: 'neutral',
+  dts: true,
+  format: ['cjs', 'esm'],
+  exports: true,
+  unbundle: true,
+});
